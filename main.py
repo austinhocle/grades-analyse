4d637aadd0840962c1aa242b9e6d3f70506178c4
austinhocle <austinhocle@gmail.com>
2025-12-22 23:56:28 +1000

    Fix dataset for further calculations

--- main.py
+++ main.py
@@ -1,11 +1,42 @@
 import pandas as pd
 
-# Load data
+from word2number import w2n
+
+# Main: Load data
 df = pd.read_csv('grades.csv')
 
-# Preview data
+# Main: Preview data
 print(df.head())
 
-# Inspect structure
+# Main: Inspect structure
 print(df.info())
 print(df.isna().sum())
+
+# Main: Clean data
+
+# -> Convert string numbers to numeric values
+def text_to_num(text):
+    try:
+        return w2n.word_to_num(text.replace('-', ' '))
+    except:
+        return text
+
+df['Score'] = df['Score'].apply(text_to_num)
+df['MaxScore'] = df['MaxScore'].apply(text_to_num)
+
+df['Score'] = pd.to_numeric(df['Score'], errors='coerce')
+df['MaxScore'] = pd.to_numeric(df['MaxScore'], errors='coerce')
+
+# -> Remove NaN entries in 'Score' and 'MaxScore' columns
+df.dropna(subset=['Score', 'MaxScore'], inplace=True)
+
+# -> Check for duplicates
+print ("Duplicates found:", df.duplicated().sum())
+
+df.drop_duplicates(inplace=True)
+
+# Main: Final data preview
+print(df.info())
+print(df.isna().sum())
+
+print(df.to_string())
